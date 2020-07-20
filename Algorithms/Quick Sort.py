7b8af0554a49831c17d4fe8fb3fc008e2bda223b
Karl Duggan <52557899+karlduggan@users.noreply.github.com>
2020-07-21 00:33:44 +0100

    Add files via upload

---
+++ Algorithms/Quick Sort.py
@@ -0,0 +1,20 @@
+def quick_sort(sequence):
+    length = len(sequence)
+    if length <= 1:
+        return sequence
+    else:
+        pivot = sequence.pop()
+
+    items_greater = []
+    items_lower = []
+
+    for item in sequence:
+        if item > pivot:
+            items_greater.append(item)
+
+        else:
+            items_lower.append(item)
+
+    return quick_sort(items_lower) + [pivot] + quick_sort(items_greater)
+
+print(quick_sort([8,3,5,4,1,7,8,9,2,0,6,4,9,11,13,2]))
